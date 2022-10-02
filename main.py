01a57960db0d574a3a5209f77de7c53fe56d771d
MElevy <deathmoonlevy@gmail.com>
2022-10-01 19:47:34 -0700

    Bug fixes + (Realism/Annoyingness)

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def runGuiThread():
 
 class Data:
     jump_power = 500
+    x_direction = 0
 
 class KeyManager:
     class Pressed:
@@ -26,6 +27,15 @@ def isYColliding(rect1, rect2):
         (rect2.y < rect1.y and rect2.y + rect2.height > rect1.y)
     )
 
+def isXColliding(rect1, rect2):
+    return (
+        (rect1.x < rect2.x and rect1.x + rect1.width > rect2.x) or
+        (rect2.x < rect1.x and rect2.x + rect2.width > rect1.x)
+    )
+
+def isColliding(rect1, rect2):
+    return isYColliding(rect1, rect2) and isXColliding(rect1, rect2)
+
 class GravityDemo(Window):
     def __init__(self):
         Window.__init__(self, width = 1000, height = 750)
@@ -33,7 +43,7 @@ class GravityDemo(Window):
         self.platform = Rectangle(
             100, 100, self.width - 200, 50, color = (50, 225, 100)
         )
-        self.platform_collider = g.SolidRect(100, 103, self.width - 200, 50)
+        self.platform_collider = Rectangle(100, 103, self.width - 200, 50)
         self.player = Rectangle(self.width / 2, self.height / 2, 50, 50)
 
         self.gManager = g.GravityManager(100)
@@ -57,15 +67,24 @@ class GravityDemo(Window):
             KeyManager.Pressed.W = False
 
     def update(self, dt):
-        if KeyManager.Pressed.A:
-            self.player.x -= 250 * dt
-        if KeyManager.Pressed.D:
-            self.player.x += 250 * dt
+        if not isColliding(self.platform_collider, self.player):
+            if Data.x_direction == 1:
+                self.player.x += 100 * dt
+            elif Data.x_direction == -1:
+                self.player.x -= 100 * dt
 
-        if not isYColliding(self.platform_collider, self.player):
             self.gManager.fall()
         else:
             self.gManager.land()
+            if KeyManager.Pressed.A:
+                self.player.x -= 250 * dt
+                Data.x_direction = -1
+            if KeyManager.Pressed.D:
+                self.player.x += 250 * dt
+                Data.x_direction = 1
+            if not (KeyManager.Pressed.A or KeyManager.Pressed.D):
+                Data.x_direction = 0
+
             if KeyManager.Pressed.W:
                 self.gManager.jump(Data.jump_power)
 
